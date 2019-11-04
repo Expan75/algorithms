3468ceaae49eea45f061cc1baf4e42587b07ff63
stigsvensson <erik.hakansson96@gmail.com>
2019-11-04 20:50:54 +0000

    finished insertionSort

--- ch2/insertionSort.py
+++ ch2/insertionSort.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-trial = [9,3,1]
+trial = [9,6,8,9,3,4,1,11,3,4,9,0]
 
 def insertionSort(A, *args, **kwargs):
+    res = A
     for j in range(1, len(A)):
         key = A[j]
         # insert A[j] into the sorted seq A[1..j-1]
         i = j-1
-        while (i > 0) and (A[i] > key):
+        while (i >= 0) and (A[i] > key):
             A[i+1] = A[i]
             i -= 1
         A[i+1] = key
-    return A
+    return res
 
 print(insertionSort(trial))
-for n in range(1,len(trial)):
-    print(n)
+
